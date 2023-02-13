a0a5f0736be543308ad25039477f774201fca928
Annette <annette.graham@sky.uk>
2023-02-13 03:06:23 +0000

    [Alistair] Aligned colours with rubikcube standard

--- src/app/types.ts
+++ src/app/types.ts
@@ -11,19 +11,9 @@ type Face = Array<Colour>;
 export type CubeData = Array<Face>;
 
 export const INITIAL_STATE: CubeData = [
+  Array<Colour>(9).fill(Colour.White),
   Array<Colour>(9).fill(Colour.Orange),
   Array<Colour>(9).fill(Colour.Green),
-  [
-    Colour.Blue,
-    Colour.Orange,
-    Colour.Blue,
-    Colour.Red,
-    Colour.White,
-    Colour.Orange,
-    Colour.Yellow,
-    Colour.Green,
-    Colour.Blue,
-  ],
   Array<Colour>(9).fill(Colour.Red),
   Array<Colour>(9).fill(Colour.Blue),
   Array<Colour>(9).fill(Colour.Yellow),
